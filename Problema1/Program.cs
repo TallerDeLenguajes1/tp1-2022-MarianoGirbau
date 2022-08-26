cd149f2c3c9708369f0bb18f56fa5c3900ac7223
Notbook-Nano <marianogirbau@gmail.com>
2022-08-26 19:20:52 -0300

    Punto 2 - Problema 1 y 2

---
+++ Problema1/Program.cs
@@ -0,0 +1,20 @@
+﻿    Console.WriteLine("Ingrese un numero ENTERO:");
+    string N = Console.ReadLine();
+try
+{
+    int Nint=Convert.ToInt32(N);
+    Console.WriteLine("EL cuadrado del numero es: " + Convert.ToString(Nint*Nint));
+
+}
+catch (System.FormatException e)
+{
+    Console.WriteLine($"Error de entrada: Ingreso un formato de numero no deseado '{N}'\n"+ e.Message);
+}
+catch (System.OverflowException e)
+{
+    Console.WriteLine($"Error de entrada: Ingreso un numero muy grande '{N}'\n"+ e.Message);
+}
+catch (System.Exception e)
+{
+    Console.WriteLine($"Error: \n"+ e.Message);
+}
---
+++ Problema2/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Ingrese un numero DIVIDENDO:");
+string A = Console.ReadLine();
+Console.WriteLine("Ingrese un numero DIVISOR:");
+string B = Console.ReadLine();
+try
+{
+    var a=Convert.ToDecimal(A);
+    var b=Convert.ToDecimal(B);
+    division(a,b);
+}
+catch (System.FormatException e)
+{
+    Console.WriteLine("Error de entrada: Ingreso un formato de numero no deseado \n"+ e.Message);
+}
+catch (System.OverflowException e)
+{
+    Console.WriteLine("Error de entrada: Ingreso un numero muy grande \n"+ e.Message);
+}
+catch (System.DivideByZeroException e)
+{
+    Console.WriteLine($"Error de entrada: Estas dividiendo en 0!! \n"+ e.Message);
+}
+
+
+void division (decimal A,decimal B){
+    Console.WriteLine("La division es: " + Convert.ToString(A/B));
+}
